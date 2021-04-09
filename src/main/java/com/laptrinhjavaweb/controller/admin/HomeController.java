91e4da0f1f13a9a1579d83fc6474cd02cc475c42
Dia <baosonheo145@gmail.com>
2021-04-09 16:55:52 +0700

    Done

--- src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
+++ src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -2,11 +2,14 @@ package com.laptrinhjavaweb.controller.admin;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.laptrinhjavaweb.model.UserModel;
 
 
 @WebServlet(urlPatterns = {"/admin-home"})
@@ -16,7 +19,8 @@ public class HomeController extends HttpServlet	{
 	private static final long serialVersionUID = -1452334356037796178L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher rd = request.getRequestDispatcher("/view/admin/home.jsp");
+		rd.forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
