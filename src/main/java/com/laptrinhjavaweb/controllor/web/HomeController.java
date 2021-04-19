0787e9d603e5a99e9e0d851db90729881dd45533
Dia <baosonheo145@gmail.com>
2021-04-20 00:20:53 +0700

    Restful API

--- src/main/java/com/laptrinhjavaweb/controllor/web/HomeController.java
+++ src/main/java/com/laptrinhjavaweb/controllor/web/HomeController.java
@@ -10,32 +10,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.laptrinhjavaweb.model.UserModel;
+import com.laptrinhjavaweb.model.NewsModel;
 import com.laptrinhjavaweb.service.ICategoryService;
 import com.laptrinhjavaweb.service.INewService;
 
+@WebServlet(urlPatterns = { "/trang-chu" })
+public class HomeController extends HttpServlet {
 
-@WebServlet(urlPatterns = {"/trang-chu"})
-public class HomeController extends HttpServlet	{
-	
 	@Inject
 	private ICategoryService categoryService;
+
 	@Inject
 	private INewService newService;
-
 	private static final long serialVersionUID = -1452334356037796178L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Long categoryId = 1L;
-		 
-		request.setAttribute("news",newService.findByCategoryId(categoryId));
 
-		request.setAttribute("categories",categoryService.findALL());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*
+		 * String title = "bai viet 4"; String content = "bai biet 4"; Long categoryId =
+		 * 1L; NewsModel newsModel=new NewsModel(); newsModel.setTilte(title);
+		 * newsModel.setContent(content); newsModel.setCategoryId(categoryId);
+		 * newService.save(newsModel); request.setAttribute("categories",
+		 * categoryService.findALL());
+		 */
 		RequestDispatcher rd = request.getRequestDispatcher("/view/web/home.jsp");
 		rd.forward(request, response);
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 }
